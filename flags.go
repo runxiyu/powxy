ee6d4c0b46cc3107c4e250cdebd289dcac7add26
Runxi Yu <me@runxiyu.org>
2025-03-23 12:50:28 +0800

    Fix source URL

--- flags.go
+++ flags.go
@@ -13,7 +13,7 @@ var (
 
 func init() {
 	flag.UintVar(&global.NeedBits, "difficulty", 17, "leading zero bits required for the challenge")
-	flag.StringVar(&global.SourceURL, "source", "https://https://forge.lindenii.runxiyu.org/powxy/:/repos/powxy/", "url to the source code")
+	flag.StringVar(&global.SourceURL, "source", "https://forge.lindenii.runxiyu.org/powxy/:/repos/powxy/", "url to the source code")
 	flag.StringVar(&listenAddr, "listen", ":8081", "address to listen on")
 	flag.StringVar(&destHost, "upstream", "http://127.0.0.1:8080", "destination url base to proxy to")
 	flag.BoolVar(&secondary, "secondary", false, "trust X-Forwarded-For headers")
