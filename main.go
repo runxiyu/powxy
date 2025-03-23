71559a48afc860d124b4ddfeb8df752b22a25053
Runxi Yu <me@runxiyu.org>
2025-03-23 12:01:36 +0800

    Separate validateBitZeros into a separate file

--- main.go
+++ main.go
@@ -197,23 +197,3 @@ func proxyRequest(writer http.ResponseWriter, request *http.Request) {
 func stringToBytes(s string) (bytes []byte) {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
-
-func validateBitZeros(bs []byte, n uint) bool {
-	q := n / 8
-	r := n % 8
-
-	for i := uint(0); i < q; i++ {
-		if bs[i] != 0 {
-			return false
-		}
-	}
-
-	if r > 0 {
-		mask := byte(0xFF << (8 - r))
-		if bs[q]&mask != 0 {
-			return false
-		}
-	}
-
-	return true
-}
