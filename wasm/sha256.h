ad79f448cb5e82cf6dfcf2ec7052add87edfc5a3
Runxi Yu <me@runxiyu.org>
2025-03-25 10:33:56 +0800

    More C style changes

--- wasm/sha256.h
+++ wasm/sha256.h
@@ -1,21 +1,14 @@
-/*********************************************************************
-* Filename:   sha256.h
-* Author:     Brad Conte (brad AT bradconte.com)
-* Copyright:  Identified to be public domain by ducky
-* Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Defines the API for the corresponding SHA1 implementation.
-*********************************************************************/
+// SPDX-FileContributor: Brad Conte <brad@bradconte.com>
+// Identified to be public domain by ducky
+// Disclaimer: This code is presented "as is" without any guarantees.
 
 #ifndef SHA256_H
 #define SHA256_H
 
-/*************************** HEADER FILES ***************************/
 #include <stddef.h>
 
-/****************************** MACROS ******************************/
 #define SHA256_BLOCK_SIZE 32	// SHA256 outputs a 32 byte digest
 
-/**************************** DATA TYPES ****************************/
 typedef unsigned char BYTE;	// 8-bit byte
 typedef unsigned int WORD;	// 32-bit word, change to "long" for 16-bit machines
 
@@ -26,7 +19,6 @@ typedef struct {
 	WORD state[8];
 } SHA256_CTX;
 
-/*********************** FUNCTION DECLARATIONS **********************/
 void sha256_init(SHA256_CTX * ctx);
 void sha256_update(SHA256_CTX * ctx, const BYTE data[], size_t len);
 void sha256_final(SHA256_CTX * ctx, BYTE hash[]);
